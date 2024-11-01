06021bfc4ed301f9a1daef6b566db88235e36b13
Asolidao <99705560+Asolidao@users.noreply.github.com>
2024-11-01 10:16:24 -0300

    1001 - Extremamente Básico

---
+++ 1001.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+
+    cin >> A >> B;
+
+    cout << "X = " << A+B << endl;;
+    return 0;
+}
+
