833a7ace9da2454d3808e46ea0e78a1028954cde
Asolidao <99705560+Asolidao@users.noreply.github.com>
2024-11-02 03:27:02 -0300

    1008 - Salário

---
+++ 1008.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    double C; 
+    cout<<setprecision(2)<<fixed;
+    cin >> A >> B >> C;
+    
+    cout << "NUMBER = " << A << endl;
+    cout << "SALARY = U$" << (B * C);
+    
+    return 0;
+}
+
