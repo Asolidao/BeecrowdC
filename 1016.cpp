e82f0ac8446c3699f54c70babe78bec03ae005c6
Asolidao <99705560+Asolidao@users.noreply.github.com>
2024-11-05 09:10:27 -0300

    1016 - Distância

---
+++ 1016.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int Y;
+
+    cin >> Y;
+
+    cout <<setprecision(4)<<fixed;
+    cout << Y * 2 <<" minutos"<< endl;
+    
+    return 0;
+}
